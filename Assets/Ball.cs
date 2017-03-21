907e248a22af5c38263f287c729c7f600b414fb2
Nyarlygames <nanacry@gmail.com>
2017-03-21 12:34:35 +0100

    Najama 1st commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    float speedx = 15f;
+    float speedz = 5f;
+    float gravity = 0.5f;
+    Vector3 dir = new Vector3(0.0f, 0.0f, 0.0f);
+    public Rigidbody bcol;
+    public Transform bsize;
+    bool flowing = true;
+    float flowingmode = 0;
+    public Transform psize;
+    public Rigidbody pcol;
+    public Transform gsize; // goal size
+    public Transform msize; // ground size
+
+    // Use this for initialization
+    void Start()
+    {
+        bcol = GetComponent<Rigidbody>();
+        bsize = GetComponent<Transform>();
+
+        GameObject player = GameObject.Find("Player1");
+        if (player != null)
+        {
+            psize = player.transform;
+            pcol = player.GetComponent<Rigidbody>();
+        }
+        GameObject ground = GameObject.Find("Ground");
+        if (ground != null)
+        {
+            msize = ground.transform;
+        }
+        GameObject goal = GameObject.Find("Goal");
+        if (goal != null)
+        {
+            gsize = goal.transform;
+        }
+        dir = new Vector3(-speedx, 0.0f, 0.0f);
+       // bcol.AddForce(dir, ForceMode.VelocityChange);
+        flowingmode = 0; // 0 start - normal line, 1 curved, 2 ??, 3 ??
+        flowing = true;
+    }
+
+    void Update()
+    {
+
+    }
+    
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+
+
+
+        /* float bosx = bsize.transform.position.x;
+         float bosz = bsize.transform.position.z;
+         float posx = psize.transform.position.x;
+         float posz = psize.transform.position.z;
+         float psizex = psize.localScale.x ;
+         float psizey = psize.localScale.z / 2;
+         float msizex = msize.localScale.x / 2;
+         float msizey = msize.localScale.z / 2;
+         float gsizex = gsize.localScale.x / 2;
+         float gsizey = gsize.localScale.z / 2;*/
+        //Debug.Log("lossy : " + msize.worldToLocalMatrix);
+        // ----------------------------------------------------- unescape ground -------------------------------------//
+        if (bsize.position.x + (bsize.localScale.x /2) > msize.localScale.x*10 / 2)
+            bsize.transform.position = new Vector3(-(msize.localScale.x *10 / 2) + (bsize.localScale.x/2), bsize.position.y, bsize.position.z);
+        if (bsize.position.x - (bsize.localScale.x /2) < -(msize.localScale.x*10 / 2))
+            bsize.transform.position = new Vector3((msize.localScale.x *10/ 2) - (bsize.localScale.x / 2), bsize.position.y, bsize.position.z);
+        
+        if (bsize.position.z - (bsize.localScale.z / 2) < -(msize.localScale.z *10 / 2))
+            bsize.transform.position = new Vector3(bsize.position.x, bsize.position.y, (msize.localScale.z * 10 / 2) -(bsize.localScale.z / 2));
+        if (bsize.position.z + (bsize.localScale.z / 2) > (msize.localScale.z *10 / 2))
+            bsize.transform.position = new Vector3(bsize.position.x, bsize.position.y, -(msize.localScale.z * 10 / 2) + (bsize.localScale.z / 2));
+
+
+        if (flowing == true)
+        {
+            float maxGravDist = 40.0f;
+            float maxGravity = 35.0f;
+            float dist;
+            Vector3 adjusteddir;
+            GameObject players = GameObject.Find("Player1");
+            Controls playergravity = players.GetComponent<Controls>();
+            switch (playergravity.zone)
+            {
+                case 1: // right
+                    adjusteddir = new Vector3(gsize.position.x + gsize.localScale.x / 2, psize.position.y, psize.position.z);
+                    break;
+                case 2: // up
+                    adjusteddir = new Vector3(psize.position.x, psize.position.y, gsize.position.z + gsize.localScale.z / 2);
+                    break;
+                case 3: // left
+                    adjusteddir = new Vector3(gsize.position.x - gsize.localScale.x / 2, psize.position.y, psize.position.z);
+                    break;
+                case 4: // down
+                    adjusteddir = new Vector3(psize.position.x, psize.position.y, gsize.position.z - gsize.localScale.z / 2);
+                    break;
+                default:
+                    adjusteddir = new Vector3(gsize.position.x, psize.position.y, gsize.localScale.z);
+                    break;
+            }
+            // GRAVITY AROUND HERE
+
+            dist = Vector3.Distance(adjusteddir, bsize.position);
+            if (dist <= maxGravDist)
+            {
+                Vector3 v = adjusteddir - bsize.position;
+                bcol.AddForce(v.normalized * (1.0f - dist / maxGravDist) * maxGravity);
+            }
+            if (flowingmode == 1)
+            {
+              /*  if (bsize.transform.position != new Vector3(20.0f, 0.0f, 0.0f))
+                {
+                    bsize.transform.position = Vector3.Slerp(bsize.transform.position, new Vector3(19.0f, 0.0f, 0.0f), 0.1f);
+                    Debug.Log("bsize " + bsize.transform.position);
+                    Debug.Log("bcol velocity " + bcol.velocity);
+                }*/
+            }
+            Debug.Log("flowing");
+        }
+
+        if (flowing == false )
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("space");
+                var rot = Quaternion.AngleAxis(psize.rotation.y, Vector3.right);
+                var lDirection = rot * Vector3.forward;
+               // var wDirection = transform.TransformDirection(lDirection);
+
+
+
+                // line ball
+                GameObject player = GameObject.Find("Player1");
+                Controls playercontrols = player.GetComponent<Controls>();
+                switch (playercontrols.zone)
+                {
+                    case 1:
+                        dir = new Vector3(speedx, 0.0f, 0.0f);
+                        break;
+                    case 2:
+                        dir = new Vector3(0.0f, 0.0f, speedz);
+                        break;
+                    case 3:
+                        dir = new Vector3(-speedx, 0.0f, 0.0f);
+                        break;
+                    case 4:
+                        dir = new Vector3(0.0f, 0.0f, -speedz);
+                        break;
+                }
+                bcol.velocity = Vector3.zero;
+                bcol.AddForce(lDirection, ForceMode.VelocityChange);
+                flowingmode = 0;
+                flowing = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                Debug.Log("c");
+                // curve ball
+                dir = new Vector3(19.0f, 0.0f, 0.0f);
+                //bsize.transform.position = Vector3.Slerp(bsize.transform.position, new Vector3 (19.0f, 0.0f, 0.0f), 0.1f);
+                bcol.AddForce(dir, ForceMode.VelocityChange);
+                flowingmode = 1;
+                flowing = true;
+            }
+            else
+            {
+                Debug.Log("sticky");
+                // replace ball on player
+                GameObject player = GameObject.Find("Player1");
+                Controls playercontrols = player.GetComponent<Controls>();
+                switch (playercontrols.zone)
+                {
+                    case 1: // right
+                        bsize.position = new Vector3(psize.position.x + psize.localScale.z/2 + bsize.localScale.x/2 +0.1f, psize.position.y, psize.position.z);
+                        break;
+                    case 2: // up
+                        bsize.position = new Vector3(psize.position.x, psize.position.y, psize.position.z + psize.localScale.z / 2 + bsize.localScale.z / 2 + 0.1f);
+                        break;
+                    case 3: // left
+                        bsize.position = new Vector3(psize.position.x - psize.localScale.z / 2 - bsize.localScale.x / 2 - 0.1f, psize.position.y, psize.position.z);
+                        break;
+                    case 4: // down
+                        bsize.position = new Vector3(psize.position.x, psize.position.y, psize.position.z - +psize.localScale.z / 2 - bsize.localScale.z / 2 - 0.1f);
+                        break;
+                }
+                flowing = false;
+            }
+        }
+    }
+
+    
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("TOUCHEE--------------------------------------------------------" + col.gameObject.name);
+        if (col.gameObject.name == "Player1")
+        {
+            switch (col.gameObject.GetComponent<Controls>().zone)
+            {
+                case 1:
+                    bsize.position = new Vector3(psize.position.x + psize.localScale.x, psize.position.y, psize.position.z);
+                    break;
+                case 2:
+                    bsize.position = new Vector3(psize.position.x, psize.position.y, psize.position.z + psize.localScale.z);
+                    break;
+                case 3:
+                    bsize.position = new Vector3(psize.position.x - psize.localScale.x, psize.position.y, psize.position.z);
+                    break;
+                case 4:
+                    bsize.position = new Vector3(psize.position.x, psize.position.y, psize.position.z - psize.localScale.z);
+                    break;
+            }
+            flowing = false;
+            Debug.Log("TOUCHEE--------------------------------------------------------");
+        }/*
+         else if (col.gameObject.name == "Goal")
+         {
+             // win
+         }*/
+    }
+}
---
+++ Assets/Controls.cs
@@ -0,0 +1,256 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public Rigidbody pcol;
+    public Rigidbody bcol;
+    public Transform psize;
+    public Transform gsize;
+    public int zone; // 1 = right / 2 = up / 3 = left / 4 = down
+
+    // Use this for initialization
+    void Start ()
+    {
+        pcol = GetComponent<Rigidbody>();
+        psize = GetComponent<Transform>();
+        psize.transform.position = new Vector3(1.5f,0.0f,0.0f);
+        GameObject goal = GameObject.Find("Goal");
+        if (goal != null)
+        {
+            gsize = goal.transform;
+        }
+        GameObject ball = GameObject.Find("Ball");
+        if (ball != null)
+        {
+            bcol = ball.GetComponent<Rigidbody>();
+        }
+        zone = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveV = Input.GetAxis("Vertical");
+
+        // up
+
+        float posx = psize.position.x;
+        float posy = psize.position.z;
+        float gposx = gsize.localScale.x/2; // scale because pos is 0,0,0
+        float gposy = gsize.localScale.z/2; // scale because pos is 0,0,0
+        float offsetx = 1.0f;//psize.localScale.x;
+        float offsety = psize.localScale.z;
+        float maxx = gposx + offsetx;
+        float maxy = gposy + offsety;
+        float speed = 0.5f;
+        float rotationspeed = 1.0f;
+        Vector3 readjusted = psize.position;
+        Vector3 rot = psize.transform.rotation.eulerAngles;
+        if (posx > maxx)
+            posx = maxx;
+        if (posx < -maxx)
+            posx = -maxx;
+        if (posy > maxy)
+            posy = maxy;
+        if (posy < -maxy)
+            posy = -maxy;
+        if ((posy < maxy) && (posy > -maxy) && ((posx != maxx) && (posx != -maxx)))
+        {
+            Debug.Log("lost inside2");
+        }
+        if ((posx < maxx) && (posx > -maxx) && ((posy != maxy) && (posy != -maxy)))
+        {
+            Debug.Log("lost inside");
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //---------------------------------------- TODO : overflow everywhere instead of - move on lane directly?-------------------------------------
+            if (posx == maxx)
+            {
+                if ((posy + (moveV * speed)) < maxy)
+                {
+                    readjusted = new Vector3(posx, 0.0f, posy + (moveV * speed)); // move up move up
+                    zone = 1;
+                }
+                else
+                {
+                    float overflow = (posy + (moveV * speed)) - maxy;
+                    // go right
+                    readjusted = new Vector3(posx - overflow, 0.0f, maxy);
+                    zone = 2;
+                }
+            }
+            else
+            {
+                if ((posy == maxy) && (posx != -maxx))
+                {
+                    if (posx + (moveV * speed) > -maxx)
+                    {
+                        //  right lane
+                        readjusted = new Vector3(posx - (moveV * speed), 0.0f, posy);
+                        zone = 2;
+                    }
+                    else
+                    {
+                        // down corner
+                        readjusted = new Vector3(-maxx, 0.0f, posy - (moveV * speed));
+                        zone = 3;
+
+                    }
+
+                }
+                else if (posx == -maxx)
+                {
+                    if ((posy - (moveV * speed)) > -maxy)
+                    {
+                        //down lane
+                        readjusted = new Vector3(posx, 0.0f, posy - (moveV * speed));
+                        zone = 3;
+                    }
+                    else
+                    {
+                        //left corner
+                        readjusted = new Vector3(posx + (moveV * speed), 0.0f, -maxy);
+                        zone = 4;
+                    }
+                }
+                else if (posy == -maxy)
+                {
+                    if ((posx + (moveV * speed)) < maxx)
+                    {
+                        //left lane
+                        readjusted = new Vector3(posx + (moveV * speed), 0.0f, posy);
+                        zone = 4;
+                    }
+                    else
+                    {
+                        //up corner
+                        readjusted = new Vector3(maxx, 0.0f, posy + (moveV * speed));
+                        zone = 1;
+                    }
+
+                }
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (posx == maxx)
+            {
+                if ((posy + (moveV * speed)) < -maxy)
+                {
+                    // go down
+                    float overflow = (posy + (moveV * speed)) - (-maxy);
+                    readjusted = new Vector3(posx + overflow, 0.0f, -maxy);
+                    zone = 4;
+                }
+                else
+                {
+                    readjusted = new Vector3(posx, 0.0f, posy + (moveV * speed)); // right to down
+                    zone = 1;
+                }
+            }
+            else
+            {
+                if ((posy == -maxy)/* && (posx != -maxx)*/) // commented double check to do everywhere?
+                {
+                    if (posx + (moveV * speed) > -maxx)
+                    {
+                        //  down lane
+                        readjusted = new Vector3(posx + (moveV * speed), 0.0f, posy);
+                        zone = 4;
+                    }
+                    else
+                    {
+                        // down to left
+                        readjusted = new Vector3(-maxx, 0.0f, posy - (moveV * speed));
+                        zone = 3;
+
+                    }
+
+                }
+                else if (posx == -maxx)
+                {
+                    if ((posy - (moveV * speed)) < maxy)
+                    {
+                        // left lane
+                        readjusted = new Vector3(posx, 0.0f, posy - (moveV * speed));
+                        zone = 3;
+                    }
+                    else
+                    {
+                        // left to up
+                        readjusted = new Vector3(posx - (moveV * speed), 0.0f, maxy);
+                        zone = 2;
+                    }
+                }
+                else if (posy == maxy)
+                {
+                    if ((posx - (moveV * speed)) < maxx)
+                    {
+                        //up lane
+                        readjusted = new Vector3(posx - (moveV * speed), 0.0f, posy);
+                        zone = 2;
+                    }
+                    else
+                    {
+                        // up to right
+                        readjusted = new Vector3(maxx, 0.0f, posy + (moveV * speed));
+                        zone = 1;
+                    }
+
+                }
+            }
+        }
+        
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rot = new Vector3(0, -rotationspeed, 0);
+            psize.transform.Rotate(rot);
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rot = new Vector3(0, rotationspeed, 0);
+            psize.transform.Rotate(rot);
+        }
+        if ((zone == 1) && (rot.y > -75) && (rot.y < 75))
+        {
+          //  Debug.Log("zone : " + zone + " rot : " + rot + " rotp : " + psize.rotation.y);
+            rot = new Vector3(0.0f, 0.0f, 0.0f);
+          //  Debug.Log("zone : " + zone + " rot : " + rot + " rotp : " + psize.rotation.y);
+            psize.transform.Rotate(rot);
+        }
+        if ((zone == 2) && (rot.y > -35) && (rot.y < -145))
+        {
+          //  Debug.Log("zone : " + zone + " rot : " + rot + " rotp : " + psize.rotation.y);
+            rot = new Vector3(0.0f, -90f, 0.0f);
+           // Debug.Log("zone : " + zone + " rot : " + rot + " rotp : " + psize.rotation.y);
+            psize.transform.Rotate(rot);
+        }
+        if ((zone == 3) && (rot.y > 130) && (rot.y < 245))
+        {
+        //    Debug.Log("zone : " + zone + " rot : " + rot + " rotp : " + psize.rotation.y);
+            rot = new Vector3(0.0f, -180f, 0.0f);
+           // Debug.Log("zone : " + zone + " rot : " + rot + " rotp : " + psize.rotation.y);
+            psize.transform.Rotate(rot);
+        }
+        if ((zone == 4) && (rot.y < 145) && (rot.y > 30))
+        {
+            rot = new Vector3(0.0f, 90f, 0.0f);
+          //  Debug.Log("zone : " + zone + " rot : " + rot);
+            psize.transform.Rotate(rot);
+        }
+        /* else
+         {
+             rot = Vector3.zero;
+         }*/
+        psize.transform.position = readjusted;
+    }
+}
